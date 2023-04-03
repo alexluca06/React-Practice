73363b43baecd34750afc4d081ae1fbd9ba24286
alexluca06 <lucaalexandru06@yahoo.com>
2023-04-03 13:33:22 +0300

    passing-thoughts app

---
+++ passing-thoughts/src/components/AddThoughtForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { generateId, getNewExpirationTime } from '../utilities/utils';
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState('');
+
+  const handleTextChange = ({target}) => {
+    setText(target.value);
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(text.length > 0) {
+      const thought = {
+        id: generateId(),
+        text: text,
+        expiresAt: getNewExpirationTime(),
+      };
+      props.addThought(thought);
+      setText('');
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit} className="AddThoughtForm">
+      <input
+      value={text}
+      onChange={handleTextChange}
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
